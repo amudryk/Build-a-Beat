4bab2424de292988820a9096e57392eda36d3194
nvarabioff <nvarabioff@users.noreply.github.com>
2020-07-19 12:55:34 -0700

    Move DNA.js to genetic_a.ja file. Simpler to have everything in one file for now.

--- src/components/DNA.js
+++ src/components/DNA.js
@@ -13,81 +13,73 @@
 // 4. Echo
 // 5. Sharpness
 
-class DNA {
+// class DNA {
 	
 	// The genetic sequence (defines a "song" in code)
-	var genes = {
-		beats:[][],
-		tempo:0,
-		beat_density:[],
-		pitch:0,
-		attack:0,
-		delay:0
-	}
+	// Current song should be live updated with the GA
+	// var genes = {
+		// beats:[][],
+		// tempo:0,			// [10,200]
+		// beat_density:[],	// [0,100]
+		// pitch:0,			// [44,100]
+		// attack:0,			// [0,1]
+		// delay:0				// [0,4]
+	// }
 	
-	var target = {
-		beats:[][],
-		tempo:0,
-		beat_density:[],
-		pitch:0,
-		attack:0,
-		delay:0
-	}
-	
-	var fitness;
-	var num_beats = 16;
-	var instruments = [1,1,1,0]	// [Kick,Snare,hi-hat,piano]
-	var count;
+	// var fitness;
+	// var num_beats = 16;
+	// var instruments = [1,1,1,0]	// [Kick,Snare,hi-hat,piano]
+	// var count;
 	
 	// Constructor (makes a random DNA)
-	DNA(num_beats, instruments) {
-		for (var i = 0; i < instruments.length; i++) {
-			if instruments[i] == 1:
-				count = 0
-				for (var j = 0; j < num_beats; j++) {
-					genes.beats[i][j] = getRndInteger(0,1);	// Generate a random number between [0,1]
-					if genes.beats[i][j] == 1:
-						count++;
-				}
-				genes.beat_density[i] = count/num_beats;
-			else:
-				for (var j = 0; j < num_beats; j++) {
-					genes.beats[i][j] = 0;
-				}
-				genes.beat_density[i] = 0;
-		}
-		genes.tempo = getRndInteger(1,200);
-		genes.pitch = getRndInteger(0,100);
-		genes.attack = getRndInteger(0,10);
-		genes.delay = getRndInteger(0,10);
-	}
+	// DNA(num_beats, instruments) {
+		// for (var i = 0; i < instruments.length; i++) {
+			// if instruments[i] == 1:
+				// count = 0
+				// for (var j = 0; j < num_beats; j++) {
+					// genes.beats[i][j] = getRndInteger(0,1);	// Generate a random number between [0,1]
+					// if genes.beats[i][j] == 1:
+						// count++;
+				// }
+				// genes.beat_density[i] = 100*(count/num_beats);
+			// else:
+				// for (var j = 0; j < num_beats; j++) {
+					// genes.beats[i][j] = 0;
+				// }
+				// genes.beat_density[i] = 0;
+		// }
+		// genes.tempo = getRndInteger(1,200);
+		// genes.pitch = getRndInteger(0,100);
+		// genes.attack = getRndInteger(0,10);
+		// genes.delay = getRndInteger(0,10);
+	// }
 	
 	// random number generator function - from https://www.w3schools.com/js/js_random.asp
-	function getRndInteger(min, max) {
-		return Math.floor(Math.random() * (max - min) ) + min;
-	}
+	// function getRndInteger(min, max) {
+		// return Math.floor(Math.random() * (max - min) ) + min;
+	// }
 	
 	// Fitness function for the initial creation - low score is more fit
 	// Fitness only calculates difference in values, not scaled. To scale, maybe divide by max
-	calcFitnessInitial(target) {
-		fitness = 0;
-		fitness = fitness + Math.abs(genes.tempo-target.tempo)
-		for (var i = 0; i < instruments.length; i++) {
-			fitness = fitness + Math.abs(genes.beat_density[i] - target.beat_density[i])
-		}
-	}
+	// calcFitnessInitial(target) {
+		// fitness = 0;
+		// fitness = fitness + Math.abs(genes.tempo-target.tempo)/200
+		// for (var i = 0; i < instruments.length; i++) {
+			// fitness = fitness + Math.abs(genes.beat_density[i] - target.beat_density[i])/100
+		// }
+	// }
 	
 	// Fitness function for a song in full
-	calcFitnessFull(target) {
-		fitness = 0;
-		fitness = fitness + Math.abs(genes.tempo-target.tempo)
-		for (var i = 0; i < instruments.length; i++) {
-			fitness = fitness + Math.abs(genes.beat_density[i] - target.beat_density[i])
-		}
-		fitness = fitness + Math.abs(genes.pitch-target.pitch)
-		fitness = fitness + Math.abs(genes.attack-target.attack)
-		fitness = fitness + Math.abs(genes.delay-target.delay)
-	}
+	// calcFitnessFull(target) {
+		// fitness = 0;
+		// fitness = fitness + Math.abs(genes.tempo-target.tempo)/200
+		// for (var i = 0; i < instruments.length; i++) {
+			// fitness = fitness + Math.abs(genes.beat_density[i] - target.beat_density[i])/100
+		// }
+		// fitness = fitness + Math.abs(genes.pitch-target.pitch)/100
+		// fitness = fitness + Math.abs(genes.attack-target.attack)/1
+		// fitness = fitness + Math.abs(genes.delay-target.delay)/4
+	// }
 	
 	
 	
